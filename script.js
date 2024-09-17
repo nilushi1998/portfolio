86a5164edb98b6c52a9567b606799cc644ee9df8
nilushi1998 <164764426+nilushi1998@users.noreply.github.com>
2024-09-17 09:37:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Example of adding interactivity
+document.addEventListener('DOMContentLoaded', function () {
+    const skills = document.querySelector('#skills');
+    skills.addEventListener('click', function () {
+        alert('Thanks for checking out my skills!');
+    });
+});
